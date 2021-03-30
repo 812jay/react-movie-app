ef9114d47306b4a7888ed08834ebd484215d02ce
812jay <yongje812@gmail.com>
2021-03-30 14:52:11 +0900

    Create Landing Page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,4 +3,6 @@ export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
 
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+export const API_KEY = '4b4b85db5ed6d7298fa7da547bd638db'
